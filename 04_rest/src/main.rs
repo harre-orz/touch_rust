d6a1d40260b3a8d82dfc1045c12eaa56de55e489
deadcheat <feat.dead.cheat+github@gmail.com>
2016-08-23 04:23:44 +0900

    add feature

--- 04_rest/src/main.rs
+++ 04_rest/src/main.rs
@@ -1,10 +1,49 @@
 extern crate rustc_serialize;
 #[macro_use]
 extern crate nickel;
+extern crate nickel_sqlite;
 
-use nickel::{Nickel, HttpRouter, JsonBody};
-use rustc_serialize::json::{Json, ToJson};
+use nickel::{
+    Nickel,
+    HttpRouter,
+    JsonBody,
+    Request,
+    Response,
+    Middleware,
+    MiddlewareResult
+};
+use nickel_sqlite::{
+    SqliteMiddleware,
+    SqliteRequestExtensions
+};
+use rustc_serialize::json::{
+    Json,
+    ToJson
+};
 use std::collections::BTreeMap;
+
+struct Logger;
+
+impl<D> Middleware<D> for Logger {
+    fn invoke<'mw, 'conn>(&self, req: &mut Request<'mw, 'conn, D>, res: Response<'mw, D>)
+    -> MiddlewareResult<'mw, D> {
+        println!("logging request from logger middleware: {:?}", req.origin.uri);
+        res.next_middleware()
+    }
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct TodoForm {
+    title:  String,
+    description: String,
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+struct TodoUpdateForm {
+    title:  String,
+    description: String,
+    status: i32,
+}
 
 #[derive(RustcDecodable, RustcEncodable)]
 struct Todo {
@@ -48,53 +87,100 @@ impl ToJson for TodoList {
 }
 
 fn main() {
+
+    let db_url  = "resources/todo.sqlite";
+    let mw      = SqliteMiddleware::new(&db_url).expect(
+        "failed to connect to database");
+
     let mut server = Nickel::new();
 
-    server.get("/todo", middleware! { |_, response|
-        // let todo = request.json_as::<Todo>().unwrap();
-        let todo1 = Todo {
-            id: 1,
-            title: "title1".to_string(),
-            description: "desc1".to_string(),
-            status: 1,
-        };
-        let todo2 = Todo {
-            id: 2,
-            title: "title2".to_string(),
-            description: "desc2".to_string(),
-            status: 0,
-        };
-        let todolist = vec![todo1, todo2];
+    server.utilize(mw);
+    server.utilize(Logger);
+
+    server.get("/todo", middleware! { |request, response|
+        let conn = request.db_conn().unwrap();
+        let mut stmt = conn.prepare(
+            "SELECT id, title, description, status FROM todo").unwrap();
+        let todo_iter = stmt.query_map(&[], |row| {
+            Todo {
+                id   : row.get(0),
+                title : row.get(1),
+                description : row.get(2),
+                status : row.get(3),
+            }
+        }).unwrap();
+
+        let mut todolist = Vec::new();
+        for todo in todo_iter {
+            todolist.push(todo.unwrap());
+        }
+
         let data = TodoList {
             data: todolist,
         };
         data.to_json()
     });
 
-    server.get("/todo/:id", middleware! { |_, response|
-        // let todo = request.json_as::<Todo>().unwrap();
-        let todo1 = Todo {
-            id: 1,
-            title: "title1".to_string(),
-            description: "desc1".to_string(),
-            status: 1,
+    server.get("/todo/:id", middleware! { |request, response|
+        let id = request.param("id").unwrap();
+        let conn = request.db_conn().unwrap();
+        let mut stmt = conn.prepare(
+            "SELECT id, title, description, status FROM todo WHERE id = ?"
+            ).unwrap();
+        let todo_iter = stmt.query_map(&[&id], |row| {
+            Todo {
+                id   : row.get(0),
+                title : row.get(1),
+                description : row.get(2),
+                status : row.get(3),
+            }
+        }).unwrap();
+
+        let mut todolist = Vec::new();
+        for todo in todo_iter {
+            todolist.push(todo.unwrap());
+        }
+
+        let data = TodoList {
+            data: todolist,
         };
-        todo1.to_json()
+        data.to_json()
     });
 
     server.post("/todo", middleware! { |request, response|
-        let todo = request.json_as::<Todo>().unwrap();
-        format!("You send title: {} desc: {}", todo.title, todo.description)
+        let todo = request.json_as::<TodoForm>().unwrap();
+        let conn = request.db_conn().unwrap();
+        match conn.execute("INSERT INTO todo (title, description, status)
+                      VALUES ($1, $2, $3)",
+                     &[&todo.title, &todo.description, &"0"]) {
+            Ok(_)  => format!("You add id: {} title: {} desc: {}",
+             conn.last_insert_rowid(), todo.title, todo.description),
+            Err(e) => format!("You failed add todo {} ", e)
+        }
     });
 
     server.delete("/todo/:id", middleware! { |request, response|
         let id = request.param("id").unwrap();
-        format!("You delete id {}", id.to_string())
+        let conn = request.db_conn().unwrap();
+        match conn.execute("DELETE FROM todo WHERE id = $1",
+            &[&id]) {
+            Ok(_) => format!("delete {} success", id),
+            Err(e) => format!("You failed delete todo {} ", e)
+        }
     });
 
     server.put("/todo/:id", middleware! { |request, response|
+        let todo = request.json_as::<TodoUpdateForm>().unwrap();
         let id = request.param("id").unwrap();
-        format!("You change id {}", id.to_string())
+        let conn = request.db_conn().unwrap();
+        match conn.execute("UPDATE todo SET title = $1,
+                             description = $2,
+                             status = $3 WHERE id = $4",
+                     &[&todo.title, &todo.description, &todo.status, &id]) {
+            Ok(_)  => format!("You update id: {} title: {} desc: {} status: {}",
+             id, todo.title, todo.description, todo.status),
+            Err(e) => format!("You failed edit todo {} ", e)
+        }
     });
 
     server.listen("127.0.0.1:3000");
